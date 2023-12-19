04fab861a3854e64b78f08e95fd0bc85cc367ac9
Hacktivator <118433308+Hackt1vator@users.noreply.github.com>
2023-12-19 10:09:43 +0100

    Delete ev3_remote_control directory

--- ev3_remote_control/setup_brick.py
+++
@@ -1,6 +0,0 @@
-import os
-from pybricks.hubs import EV3Brick
-
-def setup_name(name: str):
-    os.system("sudo hostnamectl set-hostname <string>")
-    #sudo hostnamectl set-hostname autonomous-vehicle2
